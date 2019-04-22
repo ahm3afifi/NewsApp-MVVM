e769b6653387167c86db99cf0a1327f69df2c335
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-04-22 18:31:14 +0200

    Headlines Cells

---
+++ TheWallNews/Cells/NewsHeadlineTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsHeadlineTableViewCell.swift
+//  thewallnews
+//
+//  Created by Ahmed Afifi on 4/22/19.
+//  Copyright © 2019 Ahmed Afifi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsHeadlineTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var headlineImageView: UIImageView!
+    
+}

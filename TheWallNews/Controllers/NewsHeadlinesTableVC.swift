b92a7288754f68b1deeb94a14613174551b37a77
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-04-23 02:43:20 +0200

    Testing UITableview

--- TheWallNews/Controllers/NewsHeadlinesTableVC.swift
+++ TheWallNews/Controllers/NewsHeadlinesTableVC.swift
@@ -11,17 +11,36 @@ import UIKit
 
 class NewsHeadlinesTableVC: UITableViewController {
     
+    let news = [["title":"This is the title", "description":"This is the description This is the description"],["title":"This is the title This is the title", "description":"This is the description This is the description This is the description This is the description This is the description This is the description"],["title":"This is the title This is the title This is the title This is the title", "description":"This is the description This is the description This is the description This is the description This is the description This is the description This is the description This is the description" ]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
-        
     }
     
     private func setupUI() {
-        
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.news.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
         
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsHeadlineTableViewCell", for: indexPath) as? NewsHeadlineTableViewCell else {
+            fatalError("NewsHeadlineTableViewCell not found")
+        }
+            
+        cell.titleLabel.text = self.news[indexPath.row]["title"]
+        cell.descriptionLabel.text = self.news[indexPath.row]["description"]
+            
+        return cell
     }
     
 }

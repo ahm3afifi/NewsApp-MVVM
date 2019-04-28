90235adc4364457bf9205eac789e87145329c0c7
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-04-29 00:20:12 +0200

    adding Models

--- TheWallNews/Managers/ThemeManager.swift
+++ TheWallNews/Managers/ThemeManager.swift
@@ -13,10 +13,9 @@ class ThemeManager {
     
     static func setup() {
         
-        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
-        
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.7978795171, green: 0.860489428, blue: 0.8768620491, alpha: 1)
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Times-Bold", size: 33)!, NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Times-Bold", size: 18)!, NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)]
     }
     
 }
---
+++ TheWallNews/Models/Category.swift
@@ -0,0 +1,22 @@
+//
+//  Category.swift
+//  thewallnews
+//
+//  Created by Ahmed Afifi on 4/28/19.
+//  Copyright © 2019 Ahmed Afifi. All rights reserved.
+//
+
+import Foundation
+
+
+struct Category {
+    
+    let title: String
+    let articles: [Article]
+    
+    // this function will return the string array with the titles for the categories
+    static func allCategories() -> [String] {
+        return ["Business","Entertainment","General","Sports"]
+    }
+    
+}
